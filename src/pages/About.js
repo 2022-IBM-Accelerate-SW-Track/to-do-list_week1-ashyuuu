25bc6bc5b0d92cfcec40f578465f57fcc0b8dde1
ashyuuu <99298060+ashyuuu@users.noreply.github.com>
2022-06-12 19:30:04 -0700

    first attempt
    did it on the wrong clone repository at first so pasted over (first attempt)

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+// import <profile_pic> from "../assets/profile_pic.jpg";
+// ^not sure why it expects a ; even though i have one.. 
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              // Image goes here
+              src = "../assets/profile_pic.jpg"
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Ashley Yu</div>
+            <div className="brief_description">
+              Hi! My name is Ashley. 
+              - I am a second year computer science and engineering major at UC Irvine. 
+              - I was born in New York but raised in Taipei (where I will be for the program!). 
+              - I love KPOP and my favorite groups are BLACKPINK and ITZY!
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
